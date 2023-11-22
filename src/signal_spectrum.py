6a5dc4fe33488541b8d15ca6b2bb55a3543eaa69
Oziel Ferreira da Silva <oziel@unifei.edu.br>
2023-11-22 20:36:27 -0300

    Código mesclado em apenas um arquivo

--- src/signal_spectrum.py
+++ src/signal_spectrum.py
@@ -1,60 +1,107 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pyaudio
+from scipy.io import wavfile
+import librosa
+import librosa.display
+import glob
 
-# Define o tempo de gravação e a taxa de amostragem
-record_time = float(input('Defina o tempo de gravação: '))
-sampling_rate = int(input('Defina a taxa de amostragem: '))
+def gravar_audio(file_name, sampling_rate, record_time=5):
+    chunk = 1024
+    formato = pyaudio.paInt16
+    canais = 1
 
-# Configura a gravação com pyaudio
-chunk = 1024
-formato = pyaudio.paInt16
-canais = 1
+    p = pyaudio.PyAudio()
+    stream = p.open(format=formato,
+                    channels=canais,
+                    rate=sampling_rate,
+                    input=True,
+                    frames_per_buffer=chunk)
 
-p = pyaudio.PyAudio()
-stream = p.open(format=formato,
-                channels=canais,
-                rate=sampling_rate,
-                input=True,
-                frames_per_buffer=chunk)
+    frames = []
 
-print("Comece a falar.")
-frames = []
+    print("Comece a falar.")
+    for i in range(0, int(sampling_rate / chunk * record_time)):
+        data = stream.read(chunk)
+        frames.append(np.frombuffer(data, dtype=np.int16))
 
-for i in range(0, int(sampling_rate / chunk * record_time)):
-    data = stream.read(chunk)
-    frames.append(np.frombuffer(data, dtype=np.int16))
+    print("Pare de falar.")
 
-print("Pare de falar.")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    vetor_gravacao = np.concatenate(frames)
+    wavfile.write(file_name, sampling_rate, vetor_gravacao)
 
-# Converte os frames para um array numpy
-vetorGravacao = np.concatenate(frames)
+    return vetor_gravacao
 
-# Cria o vetor de tempo
-periodoDeAmostragem = 1 / sampling_rate
-x_axis = np.arange(0, len(vetorGravacao)) * periodoDeAmostragem
+def encontrar_proximo_numero():
+    audio_files = glob.glob("audio_*.wav")
+    numeros = [int(file.split("_")[1].split(".")[0]) for file in audio_files]
+    if numeros:
+        return max(numeros) + 1
+    else:
+        return 1
 
-# Plota o sinal gravado
-plt.subplot(2, 1, 1)
-plt.plot(x_axis, vetorGravacao)
-plt.title("Sinal adquirido na entrada")
-plt.xlabel("Tempo (s)")
-plt.ylabel("Amplitude")
+def plotar_sinal_temporal(vetor_gravacao, sampling_rate):
+    periodo_amostragem = 1 / sampling_rate
+    x_axis = np.arange(0, len(vetor_gravacao)) * periodo_amostragem
 
-# Calcula e plote a FFT
-saidaFFT = np.abs(np.fft.fftshift(np.fft.fft(vetorGravacao)))
-eixoX_FFT = np.fft.fftshift(np.fft.fftfreq(len(vetorGravacao), periodoDeAmostragem))
+    plt.subplot(2, 2, 1)
+    plt.plot(x_axis, vetor_gravacao)
+    plt.title("Sinal adquirido na entrada")
+    plt.xlabel("Tempo (s)")
+    plt.ylabel("Amplitude")
 
-indice_central = len(eixoX_FFT) // 2
+def plotar_fft(vetor_gravacao, sampling_rate):
+    periodo_amostragem = 1 / sampling_rate
+    eixoX_FFT = np.fft.fftshift(np.fft.fftfreq(len(vetor_gravacao), periodo_amostragem))
 
-plt.subplot(2, 1, 2)
-plt.plot(eixoX_FFT[indice_central:], saidaFFT[indice_central:], linewidth=3)
-plt.title("Magnitude Complexa do Espectro da FFT")
-plt.xlabel("f (Hz)")
-plt.ylabel("Módulo FFT")
-plt.subplots_adjust(hspace=0.5)
-plt.show()
+    indice_central = len(eixoX_FFT) // 2
+
+    saida_fft = np.abs(np.fft.fftshift(np.fft.fft(vetor_gravacao)))
+
+    plt.subplot(2, 2, 3)
+    plt.plot(eixoX_FFT[indice_central:], saida_fft[indice_central:], linewidth=3)
+    plt.title("Magnitude Complexa do Espectro da FFT")
+    plt.xlabel("f (Hz)")
+    plt.ylabel("Módulo FFT")
+    plt.subplots_adjust(hspace=0.5)
+
+def plotar_espectrograma(file_name):
+    y, sr = librosa.load(file_name)
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+
+    plt.subplot(1, 2, 2)
+    librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Espectrograma')
+    plt.xlabel('Tempo (s)')
+
+def main():
+    record_time = float(input('Defina o tempo de gravação: '))
+    sampling_rate = int(input('Defina a taxa de amostragem: '))
+
+    audio_count = encontrar_proximo_numero()
+    file_name = f'audio_{audio_count}.wav'
+
+    # Gravação do áudio
+    vetor_gravacao = gravar_audio(file_name, sampling_rate, record_time)
+
+    # Define o tamanho da figura
+    plt.figure(figsize=(12, 8))
+
+    # Plotagem do sinal temporal
+    plotar_sinal_temporal(vetor_gravacao, sampling_rate)
+
+    # Plotagem da FFT
+    plotar_fft(vetor_gravacao, sampling_rate)
+
+    # Plotagem do espectrograma
+    plotar_espectrograma(file_name)
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
